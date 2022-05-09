3fcc533fe58e549b1688eae96f6befed82971d79
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-09 07:49:42 -0500

    Saludo de bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
   public static void main (String [] args){
     System.out.println("Hola Mundo");
+    System.out.println("Bienvenidos a mi proyecto personal, espero que les sea de mucha ayuda");
   }
 }
