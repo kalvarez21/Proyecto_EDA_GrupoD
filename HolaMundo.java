9bd88600fdcf327380f717feb51f1fe8f77aa06a
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-05-09 07:51:36 -0500

    Nombre y correo del autor

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
   public static void main (String [] args){
     System.out.println("Hola Mundo");
     System.out.println("Bienvenidos a mi proyecto personal, espero que les sea de mucha ayuda");
+    System.out.println("Autor: Kevin J. Alvarez Astete. Correo: kalvarez@unsa.edu.pe");
   }
 }
